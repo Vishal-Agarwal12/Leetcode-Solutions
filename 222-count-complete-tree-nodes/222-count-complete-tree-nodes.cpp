c2dcdbfa96390c8986002dae6c5cfabb0606247d
Vishal Agarwal <83177871+Vishal-Agarwal12@users.noreply.github.com>
2022-10-04 19:02:25 +0530

    Time: 81 ms (7.11%), Space: 30.9 MB (20.30%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -11,18 +11,32 @@
  */
 class Solution {
 public:
+    int findHeightLeft(TreeNode* node) {
+        int hght=0;
+        while(node) {
+            hght++;
+            node = node->left;
+        }
+        return hght;
+    }
     
-    void inorder(TreeNode* root, int &cnt) {
-        if(root!=NULL) {
-            inorder(root->left,cnt);
-            cnt++;
-            inorder(root->right,cnt);
-        }  
+    int findHeightRight(TreeNode* node) {
+        int hght=0;
+        while(node) {
+            hght++;
+            node = node->right;
+        }
+        return hght;
     }
     
     int countNodes(TreeNode* root) {
-        int cnt=0;
-        inorder(root, cnt);
-        return cnt;
+        if(root == NULL) return 0;
+        
+        int lh = findHeightLeft(root);
+        int rh = findHeightRight(root);
+        
+        if(lh==rh) pow(2,lh)-1;
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
