2d8cbfd3e37a71eff8f2c0c7dd73826d835ddc3c
Vishal Agarwal <83177871+Vishal-Agarwal12@users.noreply.github.com>
2022-10-23 16:20:42 +0530

    Time: 136 ms (66.91%), Space: 10.5 MB (89.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -9,9 +9,13 @@ public:
         for(int i=0;i<size+1;i++) dp[i][0]=0;
         for(int i=0;i<sum+1;i++) dp[0][i]=INT_MAX-1;
         
-        // for(int i=1)
+        for(int i=1;i<sum+1;i++) {
+            if(i%coins[0] == 0)
+                dp[1][i]=i/coins[0];
+            else dp[1][i] = INT_MAX-1;
+        }
         
-        for(int i=1;i<size+1;i++) {
+        for(int i=2;i<size+1;i++) {
             for(int j=1;j<sum+1;j++) {
                 if(coins[i-1]<=j) {
                     dp[i][j]=min(dp[i][j-coins[i-1]] + 1 ,
